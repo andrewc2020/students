17f3c99d52659dd9ed0d53ea04a18111a3333235
andrewc2020 <andrewc2020@gmail.com>
2019-12-11 09:13:52 +0000

    added non admin user to decode statement

--- src/middleware/non_admin_auth.js
+++ src/middleware/non_admin_auth.js
@@ -14,7 +14,7 @@ module.exports = function(req, res, next) {
     const decoded = jwt.verify(token, process.env.my_private_key);
     req.user = decoded;
     
-    console.log("decoded user {0}",req.user);
+    console.log("decoded non admin user {0}",req.user);
   //  if(!req.user.isAdmin && req.user._id != req.params.id){
   //    return res.status(401).send("Not authorised")
   //  }
